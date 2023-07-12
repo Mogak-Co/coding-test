a03791943ef26790e0d876864d5b8b917416fadd
hyzzzzy <shinhj4068@naver.com>
2023-07-12 21:31:54 +0900

    성격 유형 검사하기 / level 1

--- 신혜지/[PGS]118666.js
+++ 신혜지/[PGS]118666.js
@@ -1,6 +1,6 @@
 function solution(survey, choices) {
   let answer = '';
-  const { R, T, C, F, J, M, A, N } = { R: 0, T: 0, C: 0, F: 0, J: 0, M: 0, A: 0, N: 0 };
+  const mbti = { R: 0, T: 0, C: 0, F: 0, J: 0, M: 0, A: 0, N: 0 };
 
   choices.forEach((choice, i) => {
     if (choice <= 4) {
@@ -10,6 +10,7 @@ function solution(survey, choices) {
     }
   });
 
+  const { R, T, C, F, J, M, A, N } = mbti;
   answer += R >= T ? 'R' : 'T';
   answer += C >= F ? 'C' : 'F';
   answer += J >= M ? 'J' : 'M';
