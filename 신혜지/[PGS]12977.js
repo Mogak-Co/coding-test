121b445c18bc9accb9e569ac4ec6e5164f4fae71
hyzzzzy <shinhj4068@naver.com>
2023-07-19 02:46:20 +0900

    소수 구하기 / level 1

---
+++ 신혜지/[PGS]12977.js
@@ -0,0 +1,26 @@
+function isPrime(num) {
+  if (num < 2) return false;
+  for (let i = 2; i * i <= num; i++) {
+    if (num % i == 0) return false;            
+  }
+  return true;
+}
+
+function solution(nums) {
+  let answer = 0;
+  
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        if (isPrime(nums[i] + nums[j] + nums[k])) {
+          answer++;
+        }
+      }
+    }
+  }
+
+  return answer;
+}
+
+var a = [1,2,3,4];
+console.log(solution(a));
