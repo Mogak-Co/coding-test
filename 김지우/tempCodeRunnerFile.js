f94787f5870eb16b707b5740968bd0c5427aaebb
김지우 <ziuss76@gmail.com>
2023-08-22 11:33:26 +0900

    숫자 야구 / 실버 3

--- 김지우/tempCodeRunnerFile.js
+++ 김지우/tempCodeRunnerFile.js
@@ -1,1 +1,15 @@
 
+  }
+  return cnt;
+};
+
+let allNums = [];
+for (let i = 111; i <= 999; i++) {
+  allNums.push(i);
+}
+
+for (let turn of arr) {
+  let [guess, strike, ball] = turn;
+  allNums = allNums.filter((num) => getStrike(num, guess) === strike && getBall(num, guess) === ball);
+}
+return allNums.length;
