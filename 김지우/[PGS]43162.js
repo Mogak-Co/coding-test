10ce276982165085f1dbd72cf7a464b4fdd49ddb
김지우 <ziuss76@gmail.com>
2023-08-28 23:01:44 +0900

    레벨 3 / 네트워크

--- 김지우/[PGS]43162.js
+++ 김지우/[PGS]43162.js
@@ -1,20 +1,21 @@
 function solution(n, computers) {
   let ans = 0;
-  let arr = computers;
-  let visited = new Array(n).fill(false);
+  let visited = Array.from({ length: n }, () => false);
 
   function dfs(i) {
-    if (visited[i] === true) return 0;
-    else visited[i] = true;
-
-    for (let j in arr[i]) {
-      if (arr[i][j] == 1) dfs(j);
+    visited[i] = true;
+    for (let j = 0; j < n; j++) {
+      if (computers[i][j] === 1 && !visited[j]) {
+        dfs(j);
+      }
     }
-    return 1;
   }
 
-  for (let i in arr) {
-    ans += dfs(i);
+  for (let i = 0; i < n; i++) {
+    if (!visited[i]) {
+      dfs(i);
+      ans++;
+    }
   }
   return ans;
 }
