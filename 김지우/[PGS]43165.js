e6d70bcb168ba3009d7fc0f682ebb3ade7c03766
김지우 <ziuss76@gmail.com>
2023-08-29 18:59:21 +0900

    레벨 2 / 타겟 넘버

---
+++ 김지우/[PGS]43165.js
@@ -0,0 +1,14 @@
+function solution(numbers, target) {
+  let ans = 0;
+
+  function dfs(level, sum) {
+    if (level < numbers.length) {
+      dfs(level + 1, sum + numbers[level]);
+      dfs(level + 1, sum - numbers[level]);
+    } else {
+      if (sum === target) ans++;
+    }
+  }
+  dfs(0, 0);
+  return ans;
+}
