e4915171d133f41b25b17edcb30fb33325ee6735
김지우 <ziuss76@gmail.com>
2023-09-05 23:09:45 +0900

    토마토 / 골드 5

--- 김지우/[BJ]7576.js
+++ 김지우/[BJ]7576.js
@@ -9,41 +9,41 @@ let cnt = 0;
 let starts = [];
 for (let i = 0; i < N; i++) {
   for (let j = 0; j < M; j++) {
-    if (map[i][j] === 1) starts.push([i, j]);
+    if (map[i][j] === 1) {
+      starts.push([i, j]);
+      visited[i][j] = true;
+    }
   }
 }
 let queue = starts;
 
-function bfs(y, x) {
-  visited[y][x] = true;
+function bfs() {
   let dx = [0, 0, 1, -1];
   let dy = [1, -1, 0, 0];
   while (queue.length) {
-    const size = queue.length;
+    let size = queue.length;
+    let newQueue = [];
     for (let i = 0; i < size; i++) {
-      let [Y, X] = queue.shift();
+      let [Y, X] = queue[i];
       for (let j = 0; j < 4; j++) {
         let newY = Y + dy[j];
         let newX = X + dx[j];
         if (newY >= 0 && newX >= 0 && newY < N && newX < M && !visited[newY][newX] && map[newY][newX] === 0) {
           visited[newY][newX] = true;
           map[newY][newX] = 1;
-          queue.push([newY, newX]);
+          newQueue.push([newY, newX]);
         }
       }
     }
+    if (newQueue.length === 0) {
+      return;
+    }
     cnt++;
+    queue = newQueue;
   }
 }
-starts.forEach((start, i) => bfs(starts[i][0], starts[i][1]));
 
-// 주변 네 방향의 0을 1로 바꾸기
-// 레벨이 하나 깊어질 수록 cnt++
-// 모든 bfs 가 종료된 후 만약 map 에 0이 남아있다면 -1 출력, 아니면 cnt를 출력
+bfs();
 
-let ans = cnt > 0 ? cnt - 1 : 0;
-map.forEach((v, i) => {
-  if (v.includes(0)) ans = -1;
-});
-
+let ans = map.flat().includes(0) ? -1 : cnt;
 console.log(ans);
