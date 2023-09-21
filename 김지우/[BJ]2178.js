a7eb3eb8aad3201cc09c8c7a2f31786eb6f4ea0d
김지우 <ziuss76@gmail.com>
2023-09-21 16:36:17 +0900

    미로탐색 / 실버 1 / 리팩

--- 김지우/[BJ]2178.js
+++ 김지우/[BJ]2178.js
@@ -3,9 +3,31 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : __dirname + "/inp
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
 const [N, M] = input.shift().split(" ").map(Number);
-let map = input.map((v) => v.split("").map(Number));
-let visited = Array.from(Array(N), () => Array(M).fill(false));
+const map = input.map((line) => line.split("").map(Number));
+const visited = Array.from(Array(N), () => Array(M).fill(false));
 let ans = 0;
+
+// 맵 2차원 배열, 방문 2차원 배열, ans
+// bfs
+// 큐(시작점 넣기), 방향 배열
+
+// while (queue.length) {
+// ans++
+// 현재 큐의 크기를 저장하여 한 레벨의 원소만 처리
+
+// size 만큼 for 문
+// shift 로 하나 빼와서 [Y, X] 방문 했으면 continue
+// map[Y][X] = 0; 되돌아가기 방지
+// visited[Y][X] = true;
+
+// 4방향 for 문
+// newY, newX 만들고
+// 맵 안 범위이고 map[newY][newX] 1이면
+// queue.push([newY, newX]);
+// 만약 newY, newX 가 도착했으면, 리턴 ans++
+
+// bfs(0,0);
+// console.log(ans);
 
 function bfs(y, x) {
   const queue = [[y, x]];
@@ -14,13 +36,11 @@ function bfs(y, x) {
   while (queue.length) {
     ans++;
     const size = queue.length;
-    // 현재 큐의 크기를 저장하여 한 레벨의 원소만 처리
     for (let i = 0; i < size; i++) {
       let [Y, X] = queue.shift();
       if (visited[Y][X]) continue;
       map[Y][X] = 0;
       visited[Y][X] = true;
-
       for (let i = 0; i < 4; i++) {
         let newY = Y + dy[i];
         let newX = X + dx[i];
@@ -32,5 +52,6 @@ function bfs(y, x) {
     }
   }
 }
+
 bfs(0, 0);
 console.log(ans);
