0d0929ab2eb959181bb01651b28e1aded2de1194
김지우 <ziuss76@gmail.com>
2023-09-22 18:26:41 +0900

    유기농 배추 / 실버 2 / 리팩

--- 김지우/[BJ]1012.js
+++ 김지우/[BJ]1012.js
@@ -6,10 +6,10 @@ const T = +input.shift();
 const maps = [];
 
 for (let i = 0; i < T; i++) {
-  let [M, N, K] = input.shift().split(" ").map(Number);
-  let map = Array.from(Array(N), () => Array(M).fill(0));
+  const [M, N, K] = input.shift().split(" ").map(Number);
+  const map = Array.from(Array(N), () => Array(M).fill(0));
   for (let j = 0; j < K; j++) {
-    let [X, Y] = input.shift().split(" ").map(Number);
+    const [X, Y] = input.shift().split(" ").map(Number);
     map[Y][X] = 1;
   }
   maps.push(map);
@@ -17,19 +17,17 @@ for (let i = 0; i < T; i++) {
 
 function solution(maps) {
   const ans = [];
-
   for (let map of maps) {
-    let count = 0;
-
+    let cnt = 0;
     for (let i = 0; i < map.length; i++) {
       for (let j = 0; j < map[0].length; j++) {
         if (map[i][j]) {
-          count++; // 최초의 1만 cnt++
-          bfs(map, i, j); // bfs 에서 인접 1들을 감염시킴
+          cnt++;
+          bfs(map, i, j);
         }
       }
     }
-    ans.push(count);
+    ans.push(cnt);
   }
   return ans;
 }
@@ -38,22 +36,17 @@ function bfs(map, y, x) {
   const queue = [[y, x]];
   const dy = [1, -1, 0, 0];
   const dx = [0, 0, 1, -1];
-
-  map[y][x] = 0; // 냠냠
-
+  map[y][x] = 0;
   while (queue.length) {
     let size = queue.length;
     for (let i = 0; i < size; i++) {
-      const [Y, X] = queue.shift();
-
+      let [Y, X] = queue.shift();
       for (let i = 0; i < 4; i++) {
-        const newY = Y + dy[i];
-        const newX = X + dx[i];
-
+        let newY = Y + dy[i];
+        let newX = X + dx[i];
         if (newY >= 0 && newX >= 0 && newY < map.length && newX < map[0].length && map[newY][newX]) {
-          // 인접한 배추를 발견하면 냠냠 처리하고 큐에 추가
-          map[newY][newX] = 0; // 냠냠
           queue.push([newY, newX]);
+          map[newY][newX] = 0;
         }
       }
     }
