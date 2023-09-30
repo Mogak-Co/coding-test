ee2f5acdda759ba299bf45c80ed94e3d8406820e
Ji-woo Kim <ziuss76@gmail.com>
2023-09-30 19:05:04 +0900

    최단경로 / 골드 4 / 리팩

--- 김지우/[BJ]1753.js
+++ 김지우/[BJ]1753.js
@@ -5,40 +5,38 @@ let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
 const [V, E] = input.shift().split(" ").map(Number);
 const K = +input.shift();
-const graph = Array.from(Array(V + 1), () => Array());
+const graph = Array.from(Array(V + 1), () => []);
 
 for (let i = 0; i < E; i++) {
-  const [from, to, weight] = input[i].split(" ").map(Number);
-  graph[from].push({ to, weight });
+  const [from, to, cost] = input[i].split(" ").map(Number);
+  graph[from].push({ to, cost });
 }
 
-const dist = Array(V + 1).fill(Infinity);
 const visited = Array(V + 1).fill(false);
+const dist = Array(V + 1).fill(Infinity);
 
 function dijkstra(start) {
   dist[start] = 0;
 
   for (let i = 1; i <= V; i++) {
-    let min = Infinity; // 최소 비용
-    let cur = 0; // 최소 비용의 노드
+    let cost = Infinity;
+    let cur = 0;
 
     for (let j = 1; j <= V; j++) {
-      if (!visited[j] && min > dist[j]) {
-        // 방문을 안했고 최소 비용보다 작으면
-        min = dist[j];
+      if (!visited[j] && cost > dist[j]) {
+        cost = dist[j];
         cur = j;
       }
     }
 
-    visited[cur] = true; // 방문 처리
+    visited[cur] = true;
 
     for (let j = 0; j < graph[cur].length; j++) {
-      let next = graph[cur][j].to; // 다음에 방문할 노드
-      let weight = graph[cur][j].weight; // 다음에 방문할 노드로 가는 비용
+      let next = graph[cur][j].to;
+      let cost = graph[cur][j].cost;
 
-      if (dist[next] > dist[cur] + weight) {
-        // 현재까지 계산된 최단경로비용보다 작으면
-        dist[next] = dist[cur] + weight; // 최단경로비용 = 현재노드까지의 비용 + 다음 노드로 가는 비용
+      if (dist[next] > dist[cur] + cost) {
+        dist[next] = dist[cur] + cost;
       }
     }
   }
