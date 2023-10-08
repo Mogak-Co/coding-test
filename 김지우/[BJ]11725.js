ae20a02da9eaccde9c9d6d3f79d900d91d0ec914
Ji-woo Kim <ziuss76@gmail.com>
2023-10-08 19:24:31 +0900

    트리의 부모 찾기 / 실버 2

---
+++ 김지우/[BJ]11725.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const filePath =
+  process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+input = input.map((v) => v.split(" ").map(Number));
+let N = +input.shift();
+let graph = Array.from(Array(N + 1), () => []);
+let parent = Array(N + 1).fill(0);
+let result = "";
+
+for (let [from, to] of input) {
+  graph[from].push(to);
+  graph[to].push(from);
+}
+console.log(graph);
+// 이 요소의 부모는 누구이지?
+
+function dfs(start) {
+  for (let i = 0; i < graph[start].length; i++) {
+    let next = graph[start][i];
+    if (parent[next] === 0) {
+      parent[next] = start; // next의 부모는 start, 즉 6의 부모는 1
+      dfs(next);
+    }
+  }
+}
+
+dfs(1);
+
+for (let i = 2; i <= N; i++) {
+  result += parent[i] + "\n";
+}
+
+console.log(result.trim());
