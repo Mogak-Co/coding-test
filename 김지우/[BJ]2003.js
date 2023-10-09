66035243b2df4b394ba91d7802a0fdb90967c89e
Ji-woo Kim <ziuss76@gmail.com>
2023-10-09 21:36:01 +0900

    수들의 합 2 / 실버 4 / 리팩

--- 김지우/[BJ]2003.js
+++ 김지우/[BJ]2003.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt";
+const filePath =
+  process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
 const [N, M] = input.shift().split(" ").map(Number);
@@ -10,15 +11,12 @@ let ans = 0,
   end = 0,
   sum = 0;
 
-while (end <= N) {
-  if (sum < M) {
-    sum += arr[end++];
-  } else if (sum > M) {
-    sum -= arr[start++];
-  } else if (sum === M) {
-    ans++;
-    sum += arr[end++];
-  }
+while (start < N) {
+  if (sum >= M) sum -= arr[start++];
+  else if (end === N) break; // end가 N이면 더이상 더할 수 없으므로 break
+  else sum += arr[end++];
+
+  if (sum === M) ans++;
 }
 
 console.log(ans);
