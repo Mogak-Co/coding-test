b8449752c5e71522c570632dadc84ae2204a696e
김지우 <ziuss76@gmail.com>
2023-10-10 22:31:31 +0900

    나이트의 이동 / 실버 1 / 리팩

--- 김지우/[BJ]7562.js
+++ 김지우/[BJ]7562.js
@@ -5,27 +5,24 @@ let input = fs.readFileSync(filePath).toString().trim().split("\n");
 const N = +input.shift();
 
 for (let i = 0; i < N; i++) {
-  const I = +input.shift();
-  const [startY, startX] = input.shift().split(" ").map(Number);
-  const [endY, endX] = input.shift().split(" ").map(Number);
-  const result = bfs(I, startY, startX, endY, endX);
-  console.log(result);
+  let I = +input.shift();
+  let [startY, startX] = input.shift().split(" ").map(Number);
+  let [endY, endX] = input.shift().split(" ").map(Number);
+  let visited = Array.from(Array(I), () => Array(I).fill(false));
+  console.log(bfs(startY, startX, endY, endX, I, visited));
 }
 
-function bfs(I, startY, startX, endY, endX) {
-  const dy = [2, 2, -2, -2, 1, 1, -1, -1];
-  const dx = [1, -1, 1, -1, 2, -2, 2, -2];
-  const visited = Array.from(Array(I), () => Array(I).fill(false));
-  const queue = [];
-  queue.push([startY, startX, 0]);
+function bfs(startY, startX, endY, endX, I, visited) {
+  const dy = [1, 1, -1, -1, 2, 2, -2, -2];
+  const dx = [2, -2, 2, -2, 1, -1, 1, -1];
+  let queue = [[startY, startX, 0]];
   visited[startY][startX] = true;
-
   while (queue.length) {
-    const [y, x, cnt] = queue.shift();
+    let [y, x, cnt] = queue.shift();
     if (y === endY && x === endX) return cnt;
     for (let i = 0; i < 8; i++) {
-      const ny = y + dy[i];
-      const nx = x + dx[i];
+      let ny = y + dy[i];
+      let nx = x + dx[i];
       if (ny >= 0 && nx >= 0 && ny < I && nx < I && !visited[ny][nx]) {
         visited[ny][nx] = true;
         queue.push([ny, nx, cnt + 1]);
