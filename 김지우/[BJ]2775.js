b2d64cb5015e1e8b761f6d49536cb7b3ce33eadc
김지우 <ziuss76@gmail.com>
2023-10-13 23:09:00 +0900

    부녀회장이 될테야 / 브론즈 1

--- 김지우/[BJ]2775.js
+++ 김지우/[BJ]2775.js
@@ -9,12 +9,12 @@ for (let i = 0; i < T; i++) {
 
   const house = Array.from(Array(k + 1), () => Array(n + 1).fill(0));
 
-  for (let i = 0; i <= n; i++) {
+  for (let i = 1; i <= n; i++) {
     house[0][i] = i;
   }
   for (let i = 1; i <= k; i++) {
     for (let j = 1; j <= n; j++) {
-      house[i][j] = house[i - 1][j] + house[i][j - 1];
+      house[i][j] = house[i][j - 1] + house[i - 1][j];
     }
   }
   console.log(house[k][n]);
