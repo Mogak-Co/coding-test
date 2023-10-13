05fd496c56d1e50222e981e6bb8d679c0b4198ef
김지우 <ziuss76@gmail.com>
2023-10-13 13:04:50 +0900

    1, 2, 3 더하기 / 실버 3 / 리팩

--- 김지우/[BJ]9095.js
+++ 김지우/[BJ]9095.js
@@ -3,14 +3,13 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : __dirname + "/inp
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
 let T = +input.shift();
 let arr = input.map(Number);
-let max = Math.max(...arr);
 
-let dp = new Array(max + 1).fill(0);
+let dp = new Array(11).fill(0); // 11보다 작다는 조건이 있음
 dp[1] = 1;
 dp[2] = 2;
 dp[3] = 4;
 
-for (let i = 4; i <= max; i++) {
+for (let i = 4; i < 11; i++) {
   dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
 }
 
