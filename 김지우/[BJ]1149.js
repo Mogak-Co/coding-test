b71c696c5ff048462a5786d3dbb29df397122e1a
김지우 <ziuss76@gmail.com>
2023-10-14 22:43:30 +0900

    RGB거리 / 실버 1

--- 김지우/[BJ]1149.js
+++ 김지우/[BJ]1149.js
@@ -6,6 +6,7 @@ let arr = input.map((v) => v.split(" ").map(Number));
 
 let dp = Array.from(Array(T + 1), () => Array(3).fill(0));
 // 마지막 집을 R, G, B로 칠하는 각각의 경우의 최소값을 구하기
+// 마지막 집을 R로 칠할 경우, 이전 집은 G 또는 B로 칠해야 함
 
 for (let i = 0; i < T; i++) {
   dp[i + 1][0] = Math.min(dp[i][1], dp[i][2]) + arr[i][0];
