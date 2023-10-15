f609882606db61d4df9e9d0bdec7c5a366e4979c
김지우 <ziuss76@gmail.com>
2023-10-15 19:59:30 +0900

    정수 삼각형 / 실버 1

---
+++ 김지우/[BJ]1932.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+let T = +input.shift();
+let arr = input.map((v) => v.split(" ").map(Number));
+
+// 정수 삼각형
+// 맨 아래층부터 위로 올라가면서 합을 구한다.
+
+for (let i = T - 1; i > 0; i--) {
+  for (let j = 0; j < arr[i].length - 1; j++) {
+    arr[i - 1][j] += Math.max(arr[i][j], arr[i][j + 1]);
+  }
+}
+console.log(arr[0][0]);
