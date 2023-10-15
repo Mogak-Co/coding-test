b3724ae6eaab64cc296bf6b7a3d79fcf44f26b57
김지우 <ziuss76@gmail.com>
2023-10-15 17:54:09 +0900

    가장 긴 증가하는 부분 수열 / 실버 2

---
+++ 김지우/[BJ]11053.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+let T = +input.shift();
+let arr = input.shift().split(" ").map(Number);
+
+let dp = Array(T).fill(0);
+
+for (let i = 0; i < T; i++) {
+  dp[i] = 1;
+  for (let j = 0; j < i; j++) {
+    if (arr[i] > arr[j] && dp[i] < dp[j] + 1) dp[i] = dp[j] + 1;
+  }
+}
+console.log(Math.max(...dp));
