4c2e0197e10c341c82834e21704a1f40c1b8cfa5
김지우 <ziuss76@gmail.com>
2023-10-16 21:26:21 +0900

    피보나치 수 2 / 브론즈 1

---
+++ 김지우/[BJ]2748.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt";
+let input = fs.readFileSync(filePath).toString();
+let n = +input;
+
+let arr = [0, 1];
+for (let i = 2; i <= n; i++) {
+  arr.push(BigInt(arr[i - 1]) + BigInt(arr[i - 2]));
+}
+console.log(String(arr[n]));
