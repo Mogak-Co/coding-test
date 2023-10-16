a3299f81e08c5d58df22b88193133c51f9931051
김지우 <ziuss76@gmail.com>
2023-10-16 14:45:26 +0900

    1,2,3 더하기 / 실버 3 / 주석추가

--- 김지우/[BJ]9095.js
+++ 김지우/[BJ]9095.js
@@ -4,10 +4,14 @@ let input = fs.readFileSync(filePath).toString().trim().split("\n");
 let T = +input.shift();
 let arr = input.map(Number);
 
-let dp = new Array(11).fill(0); // 11보다 작다는 조건이 있음
-dp[1] = 1;
-dp[2] = 2;
-dp[3] = 4;
+let dp = Array(11).fill(0);
+dp[1] = 1; // 1
+dp[2] = 2; // 11 2
+dp[3] = 4; // 111 12 21 3
+
+// 13
+// 112 22
+// 1111 121 211 31
 
 for (let i = 4; i < 11; i++) {
   dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
