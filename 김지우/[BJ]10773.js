a1a82ea9d25584b76f9c1d2f37d00997f8a42183
김지우 <ziuss76@gmail.com>
2023-10-17 15:35:43 +0900

    제로 / 실버 4

---
+++ 김지우/[BJ]10773.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+let N = +input.shift();
+let stack = [];
+
+for (let i = 0; i < N; i++) {
+  let num = +input[i];
+  if (num === 0) stack.pop();
+  else stack.push(num);
+}
+console.log(stack.reduce((a, c) => a + c, 0));
