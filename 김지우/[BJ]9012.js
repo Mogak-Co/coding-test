2ccf9ed1b95ce5964edc1300188275ba1926caf5
김지우 <ziuss76@gmail.com>
2023-10-17 15:09:53 +0900

    괄호 / 실버 4

---
+++ 김지우/[BJ]9012.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+let T = +input.shift();
+
+for (let i = 0; i < T; i++) {
+  let str = input.shift();
+  let cnt = 0;
+  for (let val of str) {
+    if (val === "(") cnt++;
+    else cnt--;
+    if (cnt < 0) {
+      console.log("NO");
+      break;
+    }
+  }
+  if (cnt > 0) console.log("NO");
+  else if (cnt === 0) console.log("YES");
+}
