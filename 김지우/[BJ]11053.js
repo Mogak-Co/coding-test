3e7f429b148c2816ad084dba7356d36cdb41b111
김지우 <ziuss76@gmail.com>
2023-10-17 11:27:51 +0900

    가장 긴 증가하는 부분 수열 / 실버 2 / 리팩

--- 김지우/[BJ]11053.js
+++ 김지우/[BJ]11053.js
@@ -7,10 +7,13 @@ let arr = input.shift().split(" ").map(Number);
 
 let dp = Array(T).fill(0);
 
+// dp[i] = i 까지의 가장 긴 증가하는 부분 수열의 길이
+// 현재 값보다 작은 값들 중 가장 큰 dp값 + 1
+
 for (let i = 0; i < T; i++) {
   dp[i] = 1;
   for (let j = 0; j < i; j++) {
-    if (arr[i] > arr[j] && dp[j] + 1 > dp[i]) dp[i] = dp[j] + 1;
+    if (arr[i] > arr[j]) dp[i] = Math.max(dp[i], dp[j] + 1);
   }
 }
 console.log(Math.max(...dp));
