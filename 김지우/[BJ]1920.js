30fbb64f561e11e6f62d9e87c3987c8f4200d183
김지우 <ziuss76@gmail.com>
2023-10-18 15:43:28 +0900

    수 찾기 / 실버 4

--- 김지우/[BJ]1920.js
+++ 김지우/[BJ]1920.js
@@ -17,15 +17,10 @@ const binarySearch = (target) => {
   while (start <= end) {
     mid = Math.floor((start + end) / 2);
 
-    if (arr1[mid] === target) {
-      return 1;
-    } else if (arr1[mid] > target) {
-      end = mid - 1;
-    } else {
-      start = mid + 1;
-    }
+    if (arr1[mid] === target) return 1;
+    if (arr1[mid] < target) start = mid + 1;
+    if (arr1[mid] > target) end = mid - 1;
   }
-
   return 0;
 };
 
