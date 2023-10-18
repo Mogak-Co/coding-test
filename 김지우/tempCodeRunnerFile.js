afed1b42bfc167c1097574b4b42a9f9fecd4f9c9
김지우 <ziuss76@gmail.com>
2023-10-18 14:58:16 +0900

    균형잡힌 세상 / 실버 4

--- 김지우/tempCodeRunnerFile.js
+++ 김지우/tempCodeRunnerFile.js
@@ -1,9 +1,1 @@
-  if (n === 0) {
-    cnt[0]++;
-    return 0;
-  } else if (n === 1) {
-    cnt[1]++;
-    return 1;
-  } else {
-    return (fib[n] = fib[n - 1] + fib[n - 2]);
-  }
+  console.log(str);
