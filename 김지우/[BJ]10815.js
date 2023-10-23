d290d696aa350d1e32fa68b30a422652d8e71fe5
김지우 <ziuss76@gmail.com>
2023-10-23 21:24:04 +0900

    숫자 카드 / 실버 5

---
+++ 김지우/[BJ]10815.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+const filePath =
+  process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt'
+let input = fs.readFileSync(filePath).toString().trim().split('\n')
+
+const N = +input.shift()
+const arr = input.shift().split(' ').map(Number)
+const M = +input.shift()
+const arr2 = input.shift().split(' ').map(Number)
+
+let cards = new Set(arr)
+let ans = ''
+
+for (let i = 0; i < M; i++) {
+  if (cards.has(arr2[i])) ans += '1 '
+  else ans += '0 '
+}
+
+console.log(ans.trim())
