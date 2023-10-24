b0db0297279c3d551288adcb4ae67906e0fdbbc3
김지우 <ziuss76@gmail.com>
2023-10-24 13:22:19 +0900

    문자열 집합 / 실버 3

---
+++ 김지우/[BJ]14425.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+const filePath =
+  process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt'
+let input = fs.readFileSync(filePath).toString().trim().split('\n')
+
+const [N, M] = input.shift().split(' ').map(Number)
+const S = new Set()
+for (let i = 0; i < N; i++) {
+  S.add(input[i])
+}
+let cnt = 0
+
+for (let i = N; i < N + M; i++) {
+  if (S.has(input[i])) cnt++
+}
+
+console.log(cnt)
