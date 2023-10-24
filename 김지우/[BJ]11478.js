a4d55ad3a334a76c15053b044c5c3add8c748368
김지우 <ziuss76@gmail.com>
2023-10-24 16:14:14 +0900

    베스트셀러 / 실버 4

--- 김지우/[BJ]11478.js
+++ 김지우/[BJ]11478.js
@@ -1,14 +1,28 @@
-const rl = require('readline').createInterface(process.stdin)
+// const rl = require('readline').createInterface(process.stdin)
 
-rl.on('line', (input) => {
-  const set = new Set()
+// rl.on('line', (input) => {
+//   const set = new Set()
 
-  for (let i = 0; i < input.length; i++) {
-    for (let j = i; j < input.length; j++) {
-      set.add(input.slice(i, j + 1))
-    }
+//   for (let i = 0; i < input.length; i++) {
+//     for (let j = i; j < input.length; j++) {
+//       set.add(input.slice(i, j + 1))
+//     }
+//   }
+//   console.log(set.size)
+
+//   rl.close()
+// })
+
+const fs = require('fs')
+const filePath =
+  process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt'
+let input = fs.readFileSync(filePath).toString()
+
+const set = new Set()
+
+for (let i = 0; i < input.length; i++) {
+  for (let j = i; j < input.length; j++) {
+    set.add(input.slice(i, j + 1))
   }
-  console.log(set.size)
-
-  rl.close()
-})
+}
+console.log(set.size)
