8664e97a0c41fe48b3c4b2be17f55b04bed34331
김지우 <ziuss76@gmail.com>
2023-10-25 18:36:54 +0900

    포도주 시식 / 실버 1

--- 김지우/[BJ]2156.js
+++ 김지우/[BJ]2156.js
@@ -3,22 +3,21 @@ const filePath =
   process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt';
 let input = fs.readFileSync(filePath).toString().trim().split('\n');
 
-const T = +input.shift();
+const N = +input.shift();
 const wine = [];
-for (let i = 0; i < T; i++) {
+for (let i = 0; i < N; i++) {
   wine.push(+input[i]);
 }
-const dp = Array(T).fill(0);
-
+const dp = Array(N).fill(0);
 dp[0] = wine[0];
 dp[1] = wine[0] + wine[1];
-dp[2] = Math.max(wine[0] + wine[2], wine[1] + wine[2], dp[1]);
+dp[2] = Math.max(dp[1], wine[0] + wine[2], wine[1] + wine[2]);
 
-for (let i = 3; i < T; i++) {
-  let case1 = wine[i] + wine[i - 1] + dp[i - 3];
-  let case2 = wine[i] + dp[i - 2];
-  let case3 = dp[i - 1];
-
-  dp[i] = Math.max(case1, case2, case3);
+for (let i = 3; i < N; i++) {
+  dp[i] = Math.max(
+    wine[i] + wine[i - 1] + dp[i - 3],
+    wine[i] + dp[i - 2],
+    dp[i - 1]
+  );
 }
-console.log(dp[T - 1]);
+console.log(dp[N - 1]);
