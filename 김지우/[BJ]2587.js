6559e81c2ddf9c887f780fb079af5348f96dfe2b
김지우 <ziuss76@gmail.com>
2023-10-29 23:33:42 +0900

    대표값2 / 브론즈 2

--- 김지우/[BJ]2587.js
+++ 김지우/[BJ]2587.js
@@ -1,18 +1,15 @@
 const fs = require('fs');
 const filePath =
   process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt';
-let input = fs.readFileSync(filePath).toString().trim().split('\n');
+let input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split('\n')
+  .sort((a, b) => a - b);
 
-let N = +input.shift();
-// 각각의 로프에 걸리는 중량
-// 이 로프들로 들 수 있는 물체의 최대 중량
+let average = input.reduce((a, c) => a + +c, 0) / input.length;
+let mid = +input[2];
 
-let ropes = input.map(Number).sort((a, b) => b - a);
-let max = 0;
-
-for (let i = 0; i < N; i++) {
-  let weight = ropes[i] * (i + 1);
-  if (max < weight) max = weight;
-}
-
-console.log(max);
+console.log(average);
+console.log(mid);
