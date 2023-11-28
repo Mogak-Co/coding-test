e62f5328d8059c9a402bf73270d44f555a297bde
김지우 <ziuss76@gmail.com>
2023-11-29 00:52:09 +0900

    DFS와 BFS / forEach 활용

--- 김지우/[BJ]1260.js
+++ 김지우/[BJ]1260.js
@@ -4,9 +4,9 @@ const filePath =
 let input = fs.readFileSync(filePath).toString().trim().split('\n');
 
 const [N, M, V] = input.shift().split(' ').map(Number);
-const graph = Array.from(Array(N + 1), () => Array());
+const graph = Array.from(Array(N + 1), () => []);
 for (let i = 0; i < M; i++) {
-  const [from, to] = input[i].split(' ');
+  const [from, to] = input[i].split(' ').map(Number);
   graph[from].push(to);
   graph[to].push(from);
 }
@@ -21,10 +21,9 @@ function dfs(v) {
   if (visited[v]) return;
   visited[v] = true;
   dfsAns.push(v);
-  for (let i = 0; i < graph[v].length; i++) {
-    let next = graph[v][i];
+  graph[v].forEach((next) => {
     if (!visited[next]) dfs(next);
-  }
+  });
 }
 
 dfs(V);
@@ -35,18 +34,16 @@ let bfsAns = [];
 
 function bfs(v) {
   let queue = [v];
-
   while (queue.length) {
     let cur = queue.shift();
     visited[v] = true;
     bfsAns.push(cur);
-    for (let i = 0; i < graph[cur].length; i++) {
-      let next = graph[cur][i];
+    graph[cur].forEach((next) => {
       if (!visited[next]) {
         visited[next] = true;
         queue.push(next);
       }
-    }
+    });
   }
 }
 bfs(V);
