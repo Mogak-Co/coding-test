632d55160bb043ad06bc9107877bdd8ced67d683
김지우 <ziuss76@gmail.com>
2024-03-09 22:28:52 +0900

    설탕 배달 / 복습

--- 김지우/[BJ]2839.js
+++ 김지우/[BJ]2839.js
@@ -3,6 +3,8 @@ const filePath =
   process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt';
 let input = fs.readFileSync(filePath).toString();
 let N = +input;
+
+// cnt 는 5로 나누어 떨어질 때까지 3을 빼는 횟수
 let cnt = 0;
 
 while (true) {
@@ -10,8 +12,8 @@ while (true) {
     console.log(N / 5 + cnt);
     break;
   } else {
+    cnt += 1;
     N -= 3;
-    cnt++;
   }
   if (N < 0) {
     console.log(-1);
