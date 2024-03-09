90bf0117381d879239580760d921aba31b4ef808
김지우 <ziuss76@gmail.com>
2024-03-09 23:41:36 +0900

    1로 만들기 / 복습

--- 김지우/[BJ]1463.js
+++ 김지우/[BJ]1463.js
@@ -5,10 +5,12 @@ let input = fs.readFileSync(filePath).toString();
 let N = +input;
 let dp = Array(N + 1).fill(0);
 // 인덱스 = 숫자, 값 = 연산 횟수
+// 0 1 2 3 4 5 6 7 8 9 10
+// 0 0 1 1 2 3 2 3 3 2 3
 
 for (let i = 2; i <= N; i++) {
   dp[i] = dp[i - 1] + 1;
-  if (i % 2 === 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
-  if (i % 3 === 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+  if (i % 2 === 0) dp[i] = Math.min(dp[i / 2] + 1, dp[i]);
+  if (i % 3 === 0) dp[i] = Math.min(dp[i / 3] + 1, dp[i]);
 }
 console.log(dp[N]);
