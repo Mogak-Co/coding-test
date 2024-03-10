56046deee40c5fd2d959b2c89d9f82d8b7ea117e
김지우 <ziuss76@gmail.com>
2024-03-10 20:01:39 +0900

    2 x n 타일링 / 복습

--- 김지우/[BJ]11726.js
+++ 김지우/[BJ]11726.js
@@ -7,8 +7,8 @@ let T = +input;
 // 마지막이 ㅣ 일 때는 가로길이가 n - 1 인 타일
 // 마지막이 = 일 때는 가로길이가 n - 2 인 타일
 
-let arr = [1, 2];
+let dp = [1, 2];
 for (let i = 2; i < T; i++) {
-  arr.push((arr[i - 1] + arr[i - 2]) % 10007);
+  dp.push(dp[i - 1] + (dp[i - 2] % 10007));
 }
-console.log(arr[T - 1]);
+console.log(dp[T - 1]);
