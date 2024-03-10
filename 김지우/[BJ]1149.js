1351a5a58ac2c32bbdec57481d346a3614969712
김지우 <ziuss76@gmail.com>
2024-03-10 21:15:15 +0900

    RGB 거리 / 복습

--- 김지우/[BJ]1149.js
+++ 김지우/[BJ]1149.js
@@ -6,7 +6,7 @@ let T = +input.shift();
 let arr = input.map((v) => v.split(' ').map(Number));
 
 let dp = Array.from(Array(T + 1), () => Array(3).fill(0));
-// 마지막 집을 R, G, B로 칠하는 각각의 경우의 최소값을 구하기
+// dp = 마지막 집을 R, G, B로 칠하는 각각의 경우의 최소값
 // 마지막 집을 R로 칠할 경우, 이전 집은 G 또는 B로 칠해야 함
 
 for (let i = 0; i < T; i++) {
@@ -15,3 +15,11 @@ for (let i = 0; i < T; i++) {
   dp[i + 1][2] = Math.min(dp[i][0], dp[i][1]) + arr[i][2];
 }
 console.log(Math.min(...dp[T]));
+
+// R   G   B
+// 1 100 100
+// 100 1 100
+// 100 100 1
+
+// 1번 집을 R로 할 경우
+// 그럼 2번 집은 G 와 B 중 작은 것
