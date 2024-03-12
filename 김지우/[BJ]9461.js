28fa110b4c9bde72aa84078aa1714cc045051986
김지우 <ziuss76@gmail.com>
2024-03-12 15:56:08 +0900

    파도반 수열 / 복습

--- 김지우/[BJ]9461.js
+++ 김지우/[BJ]9461.js
@@ -3,15 +3,15 @@ const filePath =
   process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt';
 let input = fs.readFileSync(filePath).toString().trim().split('\n');
 
-const T = +input.shift();
+const T = input.shift();
 for (let i = 0; i < T; i++) {
-  const N = +input.shift();
-  const dp = Array(N + 1).fill(0);
+  let N = input[i];
+  let dp = Array(N).fill(0);
+  dp[0] = 1;
   dp[1] = 1;
   dp[2] = 1;
-  dp[3] = 1;
-  for (let i = 4; i <= N; i++) {
-    dp[i] = dp[i - 2] + dp[i - 3];
+  for (let j = 3; j < N; j++) {
+    dp[j] = dp[j - 3] + dp[j - 2];
   }
-  console.log(dp[N]);
+  console.log(dp[N - 1]);
 }
